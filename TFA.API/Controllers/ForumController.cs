b68b832422423a31769e0bc173df9c2461190d62
PavelKozaev <pkozaev221299@gmail.com>
2024-08-27 00:19:47 +0300

    API initial

---
+++ TFA.API/Controllers/ForumController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TFA.Storage;
+
+namespace TFA.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ForumController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(string[]))]
+        public async Task<IActionResult> GetForums([FromServices] ForumDbContext dbContext, CancellationToken cancellationToken)
+        {
+            var forumTitles = await dbContext.Forums.Select(f => f.Title).ToArrayAsync(cancellationToken);
+            return Ok(forumTitles);
+        }       
+    }
+}
---
+++ TFA.API/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TFA.Storage;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContextPool<ForumDbContext>(options => options.UseNpgsql("Host=localhost;Port=5432;Database=tfa;Username=postgres;Password=221299;Pooling=true;MinPoolSize=0;MaxPoolSize=100;Connection Idle Lifetime=60;"));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
